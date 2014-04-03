e44c4910f3d6939db1464e0c7fa2e1441abb0e50
wybflb <wybflb@163.com>
2014-04-03 10:22:30 +0800

    Initial Commit

---
+++ ALMenuBar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ALMenuBar
+//
+//  Created by Arien Lau on 14-4-3.
+//  Copyright (c) 2014年 Arien Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ALMenuBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ALMenuBar
+//
+//  Created by Arien Lau on 14-4-3.
+//  Copyright (c) 2014年 Arien Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
